717638fc79a17a5cb4643cf578f2ee81cf47b6a5
cherguihakim <cherguihakim7@gmail.com>
2024-10-03 18:15:32 -0400

    ajout d un bouton nommé fichier

--- flexrisc_ide/src/flexrisc_ide.adb
+++ flexrisc_ide/src/flexrisc_ide.adb
@@ -3,19 +3,21 @@ WITH Gtk.Main ;       USE Gtk.Main ;
 WITH Gtk.Window ;     USE Gtk.Window ; 
 WITH Gtk.Enums ;      USE Gtk.Enums;
 WITH Gtk.Button ;     USE Gtk.Button ; 
+WITH Gtk.Alignment ;  USE Gtk.Alignment ;
 
 
 procedure Flexrisc_Ide is
 
 win : Gtk_window ;
 bouton : Gtk_Button ; 
+conteneur : Gtk_Alignment ;
 loading_error : exception;
 
 BEGIN
    Init ; 
 
 
-   Gtk_New(Win) ;
+   Gtk_New(Win) ; -- Creation de la fenetre 
    win.set_title("Interface Ada");
    win.set_default_size(600,400);
    win.set_position(win_pos_center);
@@ -24,9 +26,12 @@ BEGIN
          THEN RAISE LOADING_ERROR ;
    END IF ; 
 
-   Gtk_New(bouton) ; 
+   Gtk_New(conteneur, 0.0, 0.0, 0.1, 0.0); --creation du conteneur pour mettre le bouton en haut a gauche
+   win.add(conteneur);
+
+   Gtk_New(bouton) ; -- Creation du bouton 
    bouton.set_label("Fichier");
-   win.add(bouton) ; 
+   conteneur.add(bouton);
 
    win.show_all ; 
    bouton.show ;
