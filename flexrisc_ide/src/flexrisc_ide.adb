ddf08e9d243639cfd9a0ea48fbaed457f821a18f
cherguihakim <cherguihakim7@gmail.com>
2024-10-03 17:38:23 -0400

    create the window

--- flexrisc_ide/src/flexrisc_ide.adb
+++ flexrisc_ide/src/flexrisc_ide.adb
@@ -1,4 +1,19 @@
+WITH Gtk.Main ;       USE Gtk.Main ; 
+WITH Gtk.Window ;     USE Gtk.Window ; 
+WITH Gtk.Enums ;      use Gtk.Enums;
+
+
 procedure Flexrisc_Ide is
-begin
-   null;
+
+win : Gtk_window ;
+
+BEGIN
+   Init ; 
+   Gtk_New(Win) ;
+   win.set_title("Interface Ada");
+   win.set_default_size(600,400);
+   win.set_position(win_pos_center);
+   Win.show_all ; 
+   Main ; 
+   
 end Flexrisc_Ide;
