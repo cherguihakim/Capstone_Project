b80ee9fa027613eb0206ca846eefbc1934575472
cherguihakim <cherguihakim7@gmail.com>
2024-10-03 17:50:43 -0400

    added the logo to the interface

--- flexrisc_ide/src/flexrisc_ide.adb
+++ flexrisc_ide/src/flexrisc_ide.adb
@@ -6,14 +6,24 @@ WITH Gtk.Enums ;      use Gtk.Enums;
 procedure Flexrisc_Ide is
 
 win : Gtk_window ;
+loading_error : exception;
 
 BEGIN
    Init ; 
+
+
    Gtk_New(Win) ;
    win.set_title("Interface Ada");
    win.set_default_size(600,400);
    win.set_position(win_pos_center);
    Win.show_all ; 
+
+   IF NOT win.Set_Icon_From_File("logo_interface_ada.jpg")
+         THEN RAISE LOADING_ERROR ;
+   END IF ; 
+
+
+
    Main ; 
-   
+
 end Flexrisc_Ide;
